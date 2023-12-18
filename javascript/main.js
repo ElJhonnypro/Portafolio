22cd65d431a0fa827a801d8ddce7af8276051039
Jhonathan Tabare <tabarejhonathan@gmail.com>
2023-12-18 12:59:21 -0500

    Commit a xd

---
+++ javascript/main.js
@@ -0,0 +1,21 @@
+let instagramlogo = document.getElementById('instagramlink')
+
+instagramlogo.addEventListener('mouseover', ( event )=>{
+event.target.innerHTML = 'Instagram<img src="instagram.png" class="instagramlogo">'
+})
+
+instagramlogo.addEventListener('mouseout', ( event )=>{
+    event.target.innerHTML = '<img src="instagram.png" class="instagramlogo">'
+
+})
+
+let discordlogo = document.getElementById('discordlink')
+
+discordlogo.addEventListener('mouseover', ( event )=>{
+    event.target.innerHTML = 'Discord<img src="discord.png" class="instagramlogo">'
+    })
+    
+    discordlogo.addEventListener('mouseout', ( event )=>{
+        event.target.innerHTML = '<img src="discord.png" class="instagramlogo">'
+    
+    })
